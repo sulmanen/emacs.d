8e8261c025d6c5814b0869e06155388b3bd1dbab
Samuli Ulmanen <sulmanen@Samuli-Ulmanens-MacBook-Pro.local>
2014-11-07 11:15:27 +0200

    remove solarized

--- init.el
+++ init.el
@@ -2,21 +2,10 @@
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(require 'color-theme-solarized)
+
 (setq-default indent-tabs-mode nil)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (solarized-dark)))
- '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "ebdd0a3f0914c57528ea6762038068ece2e112ca263d42ccdc594d5eeb59d3dc" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (setq mac-option-modifier nil
       mac-command-modifier 'meta
       x-select-enable-clipboard t)
@@ -32,5 +21,5 @@
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
 (setq emmet-move-cursor-between-quotes t) ;; default nil
 
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
+;;(require 'expand-region)
+;;(global-set-key (kbd "C-=") 'er/expand-region)
